737e394e7eb641787c4255770586d54a535bda31
Mikica <mikica@Mikicas-MacBook-Air.local>
2018-10-27 22:36:03 +0200

    add tensorflow

--- front/src/resources/views/App.vue
+++ front/src/resources/views/App.vue
@@ -3,20 +3,25 @@
     <app-header></app-header>
     <router-view/>
     <app-footer></app-footer>
+      <!--<Vimage src="/Users/mikica/Downloads/ids/mikica/mikica1.jpg"></Vimage>-->
   </div>
 </template>
 <script>
 	import Header from './components/layout/Header';
 	import Footer from './components/layout/Footer';
+	import Vimage from './components/Image';
     import Product from './pages/Product';
 
 	export default {
-		components: {'app-header': Header, 'app-footer': Footer, 'app-products': Product},
+		components: {'app-header': Header, 'app-footer': Footer, 'app-products': Product, Vimage},
 		created() {
 			this.$store.commit('initSocket');
 		},
 		computed: {
 
-		}
+		},
+        methods: {
+
+        }
 	}
 </script>
---
+++ front/src/resources/views/components/Image.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        props: ['src'],
+        render(h) {
+            return h('img', {
+                attrs: {
+                    src: this.src // require(this.src)
+                },
+            });
+        }
+    }
+
+</script>
