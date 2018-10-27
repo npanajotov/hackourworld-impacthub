b4bd85c8c333f4acb02c7d017e30b8ee31b9ee50
Nikola Panajotov <nikola.panajotov@mail.ru>
2018-10-27 22:34:49 +0200

    product and html

--- front/src/resources/views/App.vue
+++ front/src/resources/views/App.vue
@@ -1,22 +1,21 @@
 <template>
   <div id="app">
-    <app-header></app-header>
+    <!--<app-header></app-header>-->
     <router-view/>
-    <app-footer></app-footer>
+    <!--<app-footer></app-footer>-->
   </div>
 </template>
 <script>
 	import Header from './components/layout/Header';
 	import Footer from './components/layout/Footer';
-    import Product from './pages/Product';
+	import Product from './pages/Product';
 
 	export default {
 		components: {'app-header': Header, 'app-footer': Footer, 'app-products': Product},
 		created() {
 			this.$store.commit('initSocket');
 		},
-		computed: {
-
-		}
+		computed: {}
 	}
 </script>
+<style src="../assets/scss/app.scss" lang="scss"></style>
