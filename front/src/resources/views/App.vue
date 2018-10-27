e2004c0a90b22c6aa988e3a13d51e108e01c4dbc
Mikica <mikica@Mikicas-MacBook-Air.local>
2018-10-27 22:37:48 +0200

    fix

--- front/src/resources/views/App.vue
+++ front/src/resources/views/App.vue
@@ -2,12 +2,7 @@
   <div id="app">
     <!--<app-header></app-header>-->
     <router-view/>
-<<<<<<< HEAD
     <app-footer></app-footer>
-      <!--<Vimage src="/Users/mikica/Downloads/ids/mikica/mikica1.jpg"></Vimage>-->
-=======
-    <!--<app-footer></app-footer>-->
->>>>>>> b4bd85c8c333f4acb02c7d017e30b8ee31b9ee50
   </div>
 </template>
 <script>
